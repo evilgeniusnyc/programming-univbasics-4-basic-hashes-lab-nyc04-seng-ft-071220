859e55d214d8327e66803098115f345871704193
Steven Williams <evilgeniusnyc@me.com>
2020-07-19 17:12:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,8 @@ def pioneer
   }
 end
 
-
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  id_hash_generator = {
+    
+  }# return a hash with a key :id assigned to the provided number
 end
